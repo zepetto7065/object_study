7f6a7b5404e49aa209b725d884e0bdb1b89730c7
유상문 <sangmoon.yoo@wadiz.kr>
2022-04-10 19:50:45 +0900

    project init

---
+++ src/test/java/me/zepetto/objects/ObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zepetto.objects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
